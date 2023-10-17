9e2c91af52d9b3ec253e47aa5de8e191d9840040
Shivakant6387 <shivakantsingh900@gmail.com>
2023-10-17 11:51:37 +0530

    proposal testing

--- src/test/java/com/example/icici/lombard/IciciLombardApplicationTests.java
+++ src/test/java/com/example/icici/lombard/IciciLombardApplicationTests.java
@@ -1,13 +1,57 @@
 package com.example.icici.lombard;
 
+import com.example.icici.lombard.controller.TWProposalController;
+import com.example.icici.lombard.dto.proposal.request.TwNBProposalAddons;
+import com.example.icici.lombard.dto.proposal.request.TwProposalRequest;
+import com.example.icici.lombard.dto.proposal.response.TwProposalResponse;
+import com.example.icici.lombard.service.proposal.TWProposalService;
+import com.example.icici.lombard.service.proposal.TwNBProposalAddonsService;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 
 @SpringBootTest
 class IciciLombardApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
+    @InjectMocks
+    private TWProposalController proposalController;
+    @Mock
+    private TWProposalService proposalService;
+    @Mock
+    private TwNBProposalAddonsService twNBProposalAddonsService;
+
+    @Test
+    public void testCalculateInsurance() throws Exception {
+        TwProposalRequest request = new TwProposalRequest();
+        TwProposalResponse expectedResponse = new TwProposalResponse();
+        when(proposalService.calculateInsurance(any(TwProposalRequest.class))).thenReturn(expectedResponse);
+
+        ResponseEntity<TwProposalResponse> responseEntity = proposalController.calculateInsurance(request);
+
+        assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
+        assertEquals(expectedResponse, responseEntity.getBody());
+    }
+
+    @Test
+    public void testGetTwNBProposalAddons() throws Exception {
+        TwNBProposalAddons expectedAddons = new TwNBProposalAddons(/* create expected addons */);
+
+        when(twNBProposalAddonsService.getTwNBProposalAddons()).thenReturn(expectedAddons);
+
+        ResponseEntity<TwNBProposalAddons> responseEntity = proposalController.getTwNBProposalAddons();
+
+        assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
+        assertEquals(expectedAddons, responseEntity.getBody());
+    }
 }
